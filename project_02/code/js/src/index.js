3885073ab76cee3fb76edf53ac60420fc17afb7c
park3ye <pswpsw369@gmail.com>
2017-12-14 19:43:24 +0900

    <>

--- project_02/code/js/src/index.js
+++ project_02/code/js/src/index.js
@@ -21,8 +21,8 @@ banner.css({overflow:'hidden'});
 
 // btn
 var bannerBtn=bannerBox.children('.btn');
-var rightBtn=bannerBtn.children('button').first();
-var leftBtn=bannerBtn.children('button').last();
+var rightBtn=bannerBtn.children('button').last();
+var leftBtn=bannerBtn.children('button').first();
 
 // leftBtn
 leftBtn.on('click',function(e){
@@ -52,7 +52,18 @@ sideBtn.on('click',function(e){
   sideBox.css({right:'0'},function(){
     sideBox.removeAttr('style');
   });
-  
+     var style = parseInt(sideBox.css('right'));
+   console.log(style);
+
+  //if ( parseInt(sideBox.css('right')) == 0) { //'0px'로 안쓸거면 정수로 치환시켜야지만됨parseInt(sideBox.css('right') == '0px')
+    if (style == 0){
+    // sideBox.removeClass('style');
+     sideBox.removeAttr('style');
+    
+  }else{
+    // sideBox.addClass('style');
+     sideBox.css({right:'0'});
+  }
 });
 
 
